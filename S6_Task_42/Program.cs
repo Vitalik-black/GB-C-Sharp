ab9174036386af4d2ae647a4ff000e787f8faa0e
«vitaliy <vitalik.black86@gmail.com>
2023-06-12 23:15:21 +0500

    Add Seminar 6  C#

---
+++ S6_Task_42/Program.cs
@@ -0,0 +1,69 @@
+﻿// Задача 42: Напишите программу, которая будет преобразовывать десятичное число в двоичное.
+
+int num = Reader();
+switch (num)
+{
+    case -1:
+        Console.WriteLine("invalid number format");
+        break;
+    default:
+        int count = CountDifiner(num);
+        PrintBinary(BinaryNumConverter(num, count));
+        break;
+}
+int Reader()
+{
+    Console.WriteLine("enter your number: ");
+    if (int.TryParse(Console.ReadLine(), out int num) == false || num < 0)
+    {
+        num = -1;
+        return num;
+    }
+    else
+    {
+        return num;
+    }
+}
+
+int CountDifiner(int num)
+{
+    int count = 0;
+    while (num != 0)
+    {
+        num = num / 2;
+        count++;
+    }
+    return count;
+}
+
+int[] BinaryNumConverter(int num, int count)
+{
+    int[] binaryNum = new int[count];
+    for (int i = count - 1; i >= 0; i--)
+    {
+        int temp = num % 2;
+        num /= 2;
+        binaryNum[i] = temp;
+    }
+    return binaryNum;
+}
+
+void PrintBinary(int[] binary)
+{
+    for (int i = 0; i < binary.Length; i++)
+    {
+        Console.Write(binary[i]);
+    }
+}
+
+int num = Reader();
+switch (num)
+{
+    case -1:
+        Console.WriteLine("invalid number format");
+        break;
+    default:
+        int count = CountDifiner(num);
+        PrintBinary(BinaryNumConverter(num, count));
+        break;
+}
---
+++ S6_Task_42_3/Program.cs
@@ -0,0 +1,25 @@
+﻿int InPutNum(string message) // ввод данных
+{
+    Console.WriteLine(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+void Convert10To2(int num)
+{
+    int res = 0;
+    string showresult = string.Empty;
+    while (num > 0)
+    {
+        res = num % 2;
+        num /= 2;
+        showresult = res + showresult; // 0 + "пустое значени" // 0+0 // 0+00 // 1+000//
+    }
+    Console.WriteLine();
+    Console.WriteLine(showresult);
+
+}
+
+int a = InPutNum("Введите число");
+Convert10To2 (a);
+
+// string res1=Convert.ToString(number,2);
